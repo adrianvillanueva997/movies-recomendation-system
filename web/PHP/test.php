127b6330733e580df2bdbf0cf19168f4b598352d
unknown <laurav3797@gmail.com>
2019-05-23 14:04:51 +0200

    valoracion estrellas

--- web/PHP/test.php
+++ web/PHP/test.php
@@ -1,8 +1,5 @@
 <?php
-include_once 'user-user.php';
-include_once 'utilities.php';
+include 'user-user.php';
 $neighbours = get_neighbours(1, 0.8, 1, 20);
-#$unseen = get_unseen_movies($neighbours);
-#$ranking = make_ranking($unseen, $neighbours, 5);
-$movie_status = make_single_prediction(3, $neighbours);
-console_log($movie_status);
+$unseen = get_unseen_movies($neighbours);
+$ranking = make_ranking($unseen, $neighbours, 5);
